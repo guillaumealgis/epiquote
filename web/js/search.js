59fe2c324dad30f1598fb517a054cbea52984029
Guillaume Algis <guillaume.algis@gmail.com>
2011-09-24 18:53:28 +0200

    Added menu seletion indicator

--- web/js/search.js
+++ web/js/search.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
+  
+  var slider_pos = null;
+  
   $('#search').hide();
   $('#menu_search a').click(function (event) {
     event.preventDefault();
     
     $('#search').slideToggle(500);
-    $('#search input[type=text]').focus();
+    if ($('#search').height() <= 1)
+    {
+      // This save the horizontal background position of the indicator
+      slider_pos = $('#menu_indicator').css('background-position');
+      slider_pos = slider_pos.substr(0, slider_pos.indexOf(' '));
+      $('#menu_indicator').animate({
+        'background-position': '350px'
+      }, 500);
+      $('#search input').focus();
+    }
+    else
+    {
+      $('#menu_indicator').animate({
+        'background-position': slider_pos
+      }, 500);
+    }
   });
 });
